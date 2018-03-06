5cd223f04da3ba04708eeed3194f6c5b61b600a9
Leerw <lee.rw.cs@gmail.com>
2018-03-06 19:55:59 +0800

    first commit

---
+++ account/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.contrib.auth.models import User
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+class UserRegistationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
+    def clean_passsword2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords don\'t match.')
+        return cd['password2']

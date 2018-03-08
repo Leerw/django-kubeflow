3b507298fa8337ac10668ee17a7e301a38b77d49
Leerw <lee.rw.cs@gmail.com>
2018-03-08 20:30:20 +0800

    change django version to 1.11.11

--- account/urls.py
+++ account/urls.py
@@ -1,16 +1,15 @@
-from django.urls import path
 from django.conf.urls import url
 from django.contrib.auth.views import login, logout, logout_then_login, password_change, password_change_done, password_reset, password_reset_done, password_reset_confirm, password_reset_complete
 from . import views
 
 urlpatterns = [
     # path('login/', views.user_login, name='login'),
-    path('login/', login, name='login'),
-    path('logout/', logout, name='logout'),
-    path('logout_then_login', logout_then_login, name='logout_then_login'),
+    url(r'login/$', login, name='login'),
+    url(r'logout/$', logout, name='logout'),
+    url(r'logout_then_login/$', logout_then_login, name='logout_then_login'),
 
-    path('password-change/', password_change, name='password_change'),
-    path('password-change/done/', password_change_done, name='password_change_done'),
+    url(r'password-change/$', password_change, name='password_change'),
+    url(r'password-change/done/$', password_change_done, name='password_change_done'),
 
     url(r'^password-reset/$', password_reset, name='password_reset'), 
     url(r'^password-reset/done/$', password_reset_done, name='password_reset_done'), 
@@ -19,9 +18,9 @@ urlpatterns = [
 
     url(r'^password-reset/complete/$', password_reset_complete, name='password_reset_complete'),
 
-    path('', views.dashboard, name='dashboard'),
+    url(r'^$', views.dashboard, name='dashboard'),
 
-    path('register/', views.register, name='register'),
+    url(r'^register/$', views.register, name='register'),
 
-    path('init/', views.init, name='init'),
+    url(r'^init/$', views.init, name='init'),
 ]
--- kubeflow/settings.py
+++ kubeflow/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'easy_thumbnails',
+    'filer',
+    'mptt',
 ]
 
 MIDDLEWARE = [
